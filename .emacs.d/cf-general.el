5c2615da1c76ceed074f3c6e59f8ae605f830c53
Matt Gordon <matt@expectedbehavior.com>
2009-11-04 16:12:18 -0500

    updated cucumber (i.e. feature-mode) and set .emacs to actually load feature-mode instead of the non-existent cucumber mode

--- .emacs.d/cf-general.el
+++ .emacs.d/cf-general.el
@@ -45,7 +45,7 @@
 				("\\.gemspec$" . ruby-mode)
 				("\\.sass$" . sass-mode)
 				("\\.haml$" . haml-mode)
-				("\\.feature$" . cucumber-mode))         
+				("\\.feature$" . feature-mode))         
 			      auto-mode-alist))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -67,7 +67,6 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp-personal"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/cucumber.el"))
-
 (require 'feature-mode)
 
 (require 'linum)
@@ -193,8 +192,8 @@
 (load-file "~/.emacs.d/sass/sass-mode.el")
 (require 'sass-mode)
 
-(load-file "~/.emacs.d/minimap/minimap.el")
-(require 'minimap)
+;; (load-file "~/.emacs.d/minimap/minimap.el")
+;; (require 'minimap)
 
 (set-face-background 'flymake-errline "red4")
 (set-face-background 'flymake-warnline "dark slate blue")
@@ -618,7 +617,7 @@
 (defun projector (&optional nosplit)
   "Create a large window suitable for coding on a macbook."
   (interactive "P")
-  (my-set-mac-font "inconsolata" 18)
+  (my-set-mac-font "inconsolata" 20)
   (arrange-frame 170 45 nosplit))
 
 
