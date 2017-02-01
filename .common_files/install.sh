e7ec9a14735b05ed7aa8b1d5fc742a9598cdd277
Matthew Wade Gordon <matt@expectedbehavior.com>
2017-02-01 15:50:23 -0600

    Setup script for the common files should run automatically after being installed

--- .common_files/install.sh
+++ .common_files/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
+set -x
 
-git clone git://github.com/expectedbehavior/common-files.git &&
-  cp -fr common-files/* . &&
-  cp -fr common-files/.??* . &&
-  rm -fr common-files/ &&
-  git checkout . # fixes issues with weird permissions changes after copy
+git clone git@github.com:expectedbehavior/common-files.git
+cp -fr common-files/* .
+cp -fr common-files/.??* .
+rm -fr common-files/
+git checkout . # fixes issues with weird permissions changes after copy
+
+.common-files/setup.sh
--- .common_files/setup.sh
+++ .common_files/setup.sh
@@ -2,5 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
+brew list cask > /dev/null 2>&1 || brew install cask
+
 echo "Installing emacs packages"
-(cd .emacs.d && cask install)
+(cd ~/.emacs.d && cask install)
