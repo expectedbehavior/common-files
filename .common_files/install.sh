d95fb649ac1ba867b1e64b13a6180cb78b6382ef
Matthew Gordon <matthew.w.gordon@gmail.com>
2019-07-22 15:23:50 -0400

    Stop assuming the common files will be directly installed into your home directory
    Why is this change needed?
    --------------------------
    As part of our effort to make the common files more amenable to individual preferences, we're assuming you'll already have some dotfiles. Stow makes it easy to install some while using your current files.
    
    How does it address the issue?
    ------------------------------
    Remove the copy commands from the install script
    
    Any links to any relevant tickets, articles or other resources?
    ---------------------------------------------------------------

--- .common_files/install.sh
+++ .common_files/install.sh
@@ -3,10 +3,11 @@ set -e
 set -x
 
 cd ~
+# TODO - should be cloned into the `code` repo
 git clone git@github.com:expectedbehavior/common-files.git
-cp -fr common-files/* .
-cp -fr common-files/.??* .
-rm -fr common-files/
-git checkout . # fixes issues with weird permissions changes after copy
+# cp -fr common-files/* .
+# cp -fr common-files/.??* .
+# rm -fr common-files/
+#git checkout . # fixes issues with weird permissions changes after copy
 
-~/.common_files/script/setup
+~/common-files/script/setup
