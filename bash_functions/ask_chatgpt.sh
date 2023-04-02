3020df13bfeb432d04fcc4d74c9294ee1fac00ef
Matthew Gordon <matt@expectedbehavior.com>
2023-04-01 22:05:12 -0400

    Allow the chatgpt function to take a data argument
    Why is this change needed?
    --------------------------
    A more interesting use for command line chatgpt is the ability to
    write a file that provides significant context for a question and
    supply the entire thing. Some possibilities include
    - easily providing some code to optimize
    - accumulate a shared/sophisticated context for a question
    
    How does it address the issue?
    ------------------------------
    If there are two arguments, append the second to the first. If there's
    only one argument, just use that as the prompt.
    
    Any links to any relevant tickets, articles or other resources?
    ---------------------------------------------------------------

--- bash_functions/ask_chatgpt.sh
+++ bash_functions/ask_chatgpt.sh
@@ -6,13 +6,22 @@ function get_chatgpt_api_key {
 
 function ask_chatgpt {
   local prompt="$1"
+  local data="$2"
   local api_key="$(get_chatgpt_api_key)"
+  local prompt_input=""
+
+  if [ $# -eq 2 ]; then
+    prompt_input="$prompt: $data"
+  else
+    prompt_input="$prompt"
+  fi
+  
   local gpt="$(curl https://api.openai.com/v1/chat/completions -s \
     -H 'Content-Type: application/json' \
     -H "Authorization: Bearer $api_key" \
     -d '{
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "'"$prompt"'"}],
+        "messages": [{"role": "user", "content": "'"$prompt_input"'"}],
         "temperature": 0.7
     }' 2>&1)" # Redirect stderr to stdout
   local error_message="$(echo "$gpt" | jq -r '.error.message')"
